43b5410b86da05731037b512d99b2e3c1855324a
Guo Feng He <guofengh@nycstudents.net>
2023-09-24 16:41:30 -0400

    finished class and test code

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,26 @@
 public class Student {
-    private double gpa;
+    private double average;
     private String name;
     private int numberOfClasses;
+
+    public Student(double studentAverage, String studentName, int studentNumberOfClasses) {
+        average = studentAverage;
+        name = studentName;
+        numberOfClasses = studentNumberOfClasses;
+    }
+
+    public void passingDetection() {
+        if (average >= 65){
+            System.out.println("Student is passing with a " + average + " average.");
+        }
+        else{
+            System.out.println("Student is failing with a " + average + " average.");
+        }
+    }
+
+    public void studentInfo(){
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Average: " + average);
+        System.out.println("Number of Classes: " + numberOfClasses);
+    }
 }
--- src/StudentRunner.java
+++ src/StudentRunner.java
@@ -1,2 +1,8 @@
 public class StudentRunner {
+    public static void main(String[] args){
+        Student one = new Student(100, "Bartholomew", 10);
+
+        one.studentInfo();
+        one.passingDetection();
+    }
 }
